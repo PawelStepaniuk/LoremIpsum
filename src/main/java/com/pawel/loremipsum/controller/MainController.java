224b8274a8547559be114a6fcdd9a3e5ef822776
Pawel <stepaniuk.pawel@gmail.com>
2018-11-17 20:16:38 +0100

    Added a choice between the amount of words or paragraphs

--- src/main/java/com/pawel/loremipsum/controller/MainController.java
+++ src/main/java/com/pawel/loremipsum/controller/MainController.java
@@ -4,24 +4,27 @@ import de.svenjacobs.loremipsum.LoremIpsum;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-LoremIpsum loremIpsum = new LoremIpsum();
+    LoremIpsum loremIpsum = new LoremIpsum();
+
     @GetMapping("/")
-    public String generate(){
+    public String generate() {
         return "generator";
     }
     @GetMapping("/text")
-    public String text(@RequestParam(required = false) int words,int paragraphs,  ModelMap modelMap){
-        if(words<1){
-            modelMap.put("lorem","Podaj liczbe wyrazów do wygenerowania");
-        } else {
-            modelMap.put("lorem",loremIpsum.getWords(words));
+    public String text(@RequestParam(required = false) String word,
+                       @RequestParam(required = false) Integer amount,
+                       ModelMap modelMap) {
 
+        if (word.equals("true")) {
+            modelMap.put("lorem", loremIpsum.getWords(amount));
+        } else if (word.equals("false")){
+            modelMap.put("lorem", loremIpsum.getParagraphs(amount));
         }
+
         return "generator";
     }
 }
