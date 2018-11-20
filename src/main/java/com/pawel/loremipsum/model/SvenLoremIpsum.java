02331ce4c277bcae9b219e9cc46954a678750552
Pawel <stepaniuk.pawel@gmail.com>
2018-11-20 10:12:34 +0100

    Heroku push

--- src/main/java/com/pawel/loremipsum/model/SvenLoremIpsum.java
+++ src/main/java/com/pawel/loremipsum/model/SvenLoremIpsum.java
@@ -1,21 +1,63 @@
-package com.pawel.loremipsum.model;
+package com.pawel.loremipsum.model;//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
 import com.pawel.loremipsum.dao.LoremIpsumGenerator;
-import de.svenjacobs.loremipsum.LoremIpsum;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SvenLoremIpsum implements LoremIpsumGenerator {
+    public static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    private String[] loremIpsumWords = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.".split("\\s");
 
-    private LoremIpsum loremIpsum = new LoremIpsum();
 
-    @Override
-    public String getWords(int amount) {
-        return loremIpsum.getWords(amount);
+    public String getWords() {
+        return this.getWords(50);
     }
 
-    @Override
+    public String getWords(int amount) {
+        return this.getWords(amount, 0);
+    }
+
+    public String getWords(int amount, int startIndex) {
+        if (startIndex >= 0 && startIndex <= 49) {
+            int word = startIndex;
+            StringBuilder lorem = new StringBuilder();
+
+            for(int i = 0; i < amount; ++i) {
+                if (word == 50) {
+                    word = 0;
+                }
+
+                lorem.append(this.loremIpsumWords[word]);
+                if (i < amount - 1) {
+                    lorem.append(' ');
+                }
+
+                ++word;
+            }
+
+            return lorem.toString();
+        } else {
+            throw new IndexOutOfBoundsException("startIndex must be >= 0 and < 50");
+        }
+    }
+
+    public String getParagraphs() {
+        return this.getParagraphs(2);
+    }
+
     public String getParagraphs(int amount) {
-        return loremIpsum.getWords(amount);
+        StringBuilder lorem = new StringBuilder();
+
+        for(int i = 0; i < amount; ++i) {
+            lorem.append("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+            if (i < amount - 1) {
+                lorem.append("\n\n");
+            }
+        }
+
+        return lorem.toString();
     }
 }
